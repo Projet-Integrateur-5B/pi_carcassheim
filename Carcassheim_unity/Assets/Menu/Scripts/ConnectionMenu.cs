6789a21b4e38494f494756b3a1eaaba62bb266e2
Geissert Cedric <cedric.geissert@etu.unistra.fr>
2022-02-20 21:17:21 +0100

    Creation de compte

--- Carcassheim_unity/Assets/Menu/Scripts/ConnectionMenu.cs
+++ Carcassheim_unity/Assets/Menu/Scripts/ConnectionMenu.cs
@@ -4,85 +4,95 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ConnectionMenu : MonoBehaviour {
-    private HomeMenu home;
-    private Miscellaneous ms;
+public class ConnectionMenu : MonoBehaviour
+{
+	private HomeMenu home;
+	private Miscellaneous ms;
+	private static bool State = false;
+	// Start is called before the first frame update
+	void Start()
+	{
+		// SCRIPT :
+		ms = gameObject.AddComponent(typeof(Miscellaneous)) as Miscellaneous;
+		home = gameObject.AddComponent(typeof(HomeMenu)) as HomeMenu;
+	}
 
-    private static bool State = false;
+	// Update is called once per frame
+	void Update()
+	{
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // SCRIPT :
-        ms = gameObject.AddComponent(typeof(Miscellaneous)) as Miscellaneous;
-        home = gameObject.AddComponent(typeof(HomeMenu)) as HomeMenu;
-    }
+	public bool getState()
+	{
+		return State;
+	}
 
-    // Update is called once per frame
-    void Update() { }
+	public void setState(bool b)
+	{
+		State = b;
+	}
 
-    public bool getState() { return State; }
+	public void resetWarningTextCM()
+	{
+		GameObject tmpGO = GameObject.Find("Instructions");
+		Text tmpText = tmpGO.GetComponent<Text>();
+		ms.tryColor(tmpGO, Color.white, "f4fefe");
+		tmpText.text = "Connectez vous";
+	}
 
-    public void HideConnection()
-    {
-        ms.changeMenu(ms.FindMenu("ConnectionMenu"), ms.FindMenu("HomeMenu"));
-    }
+	public void HideConnection()
+	{
+		resetWarningTextCM();
+		ms.changeMenu(ms.FindMenu("ConnectionMenu"), ms.FindMenu("HomeMenu"));
+	}
 
-    public void Connect()
-    {
-        bool a = ms.StrCompare(GameObject.Find("InputField Email/Login")
-                                   .GetComponent<InputField>()
-                                   .text,
-            "Hello");
-        bool b = ms.StrCompare(GameObject.Find("InputField Password")
-                                   .GetComponent<InputField>()
-                                   .text,
-            "World");
-        State = a && b;
+	public void ForgottenPwdUser()
+	{
+		Application.OpenURL("https://tinyurl.com/Kakyoin-and-Polnareff");
+	}
 
-        if (State) {
-            Color newCol;
-            ms.tryColor(GameObject.Find("Instructions"), Color.white, "f4fefe");
-            GameObject.Find("Instructions").GetComponent<Text>().text
-                = "Connectez vous";
-            HideConnection();
-            ms.tryColor(
-                GameObject.Find("Etat de connexion"), Color.green, "#90EE90");
-            GameObject.Find("Etat de connexion").GetComponent<Text>().text
-                = "Connecte";
-            GameObject.Find("Etat de connexion").transform.position
-                = new Vector3(1250, 475, 0);
-            GameObject.Find("Btn Connexion").SetActive(false);
-            GameObject.Find("Btn Jouer").GetComponent<Button>().interactable
-                = true;
-            GameObject.Find("Btn Statistiques")
-                .GetComponent<Button>()
-                .interactable
-                = true;
+	public void Connected()
+	{
+		Color newCol;
+		Button tmpStat = GameObject.Find("Btn Statistiques").GetComponent<Button>();
+		Button tmpJouer = GameObject.Find("Btn Jouer").GetComponent<Button>();
+		ms.tryColor(GameObject.Find("Etat de connexion"), Color.green, "#90EE90");
+		GameObject.Find("Etat de connexion").GetComponent<Text>().text = "Connecte";
+		GameObject.Find("Etat de connexion").transform.position = new Vector3(1250, 475, 0);
+		GameObject.Find("Btn Connexion").SetActive(false);
+		tmpJouer.interactable = true;
+		tmpStat.interactable = true;
+		ColorUtility.TryParseHtmlString("#f4fefe", out newCol);
+		tmpJouer.GetComponentInChildren<Text>().color = newCol;
+		tmpStat.GetComponentInChildren<Text>().color = newCol;
+		Debug.Log("Connecté");
+	}
 
-            ColorUtility.TryParseHtmlString("#f4fefe", out newCol);
-            GameObject.Find("Btn Jouer")
-                .GetComponent<Button>()
-                .GetComponentInChildren<Text>()
-                .color
-                = newCol;
-            GameObject.Find("Btn Statistiques")
-                .GetComponent<Button>()
-                .GetComponentInChildren<Text>()
-                .color
-                = newCol;
-            Debug.Log("Connecté");
-        } else {
-            ms.randomIntColor(GameObject.Find("Instructions"));
-            GameObject.Find("Instructions").GetComponent<Text>().text
-                = "Ressaissiez votre login et votre mot de passe !";
-        }
-    }
+	public void Connect()
+	{
+		bool a = ms.StrCompare(GameObject.Find("InputField Email/Login").GetComponent<InputField>().text, "Hello");
+		bool b = ms.StrCompare(GameObject.Find("InputField Password").GetComponent<InputField>().text, "World");
+		State = a && b;
+		GameObject tmpGO = GameObject.Find("Instructions");
+		Text tmpText = tmpGO.GetComponent<Text>();
+		if (State)
+		{
+			HideConnection();
+			Connected();
+		}
+		else
+		{
+			ms.randomIntColor(tmpGO);
+			tmpText.text = "Ressaissiez votre login et votre mot de passe !";
+		}
+	}
 
-    public void CreateAccount()
-    {
-        ms.tryColor(GameObject.Find("Instructions"), Color.red, "#fcdc12");
-        GameObject.Find("Instructions").GetComponent<Text>().text
-            = "IL FAUT ENCORE LE FAIRE !";
-    }
+	public void CreateAccount()
+	{
+		GameObject tmpGO = GameObject.Find("Instructions");
+		Text tmpText = tmpGO.GetComponent<Text>();
+		ms.tryColor(tmpGO, Color.white, "f4fefe");
+		tmpText.text = "Connectez vous";
+		ms.changeMenu(ms.FindMenu("ConnectionMenu"), ms.FindMenu("CreateAccountMenu"));
+	}
 }
