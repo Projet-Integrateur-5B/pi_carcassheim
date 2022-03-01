afa8e0bce45a51b311c05e3bff44971e5cfc47e2
Sarah Emery <sarah.emery@etu.unistra.fr>
2022-03-02 00:01:06 +0100

    Ajout de l'arborescence des fichiers

---
+++ Carcassheim_unity/Assets/Affichage_InGame/Scripts/Tuile.cs
@@ -0,0 +1,24 @@
+using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tuile : MonoBehaviour
+{
+        void Start()
+        {
+
+        }
+
+        private void Update() 
+        {
+                
+        }
+
+        private void FixedUpdate()
+        {
+                
+        }
+}
