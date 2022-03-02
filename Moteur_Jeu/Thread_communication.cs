6160c1ad3b6de1876e1380cd70433004cf025660
Axel Grimmer <axel.grimmer@etu.unistra.fr>
2022-03-03 00:00:46 +0100

    Choix de créer un objet thread_com par thread de communication, qu'il sera le seul à modifier. Cet objet est stocké dans une liste du serveur_main

--- Moteur_Jeu/Thread_communication.cs
+++ Moteur_Jeu/Thread_communication.cs
@@ -6,17 +6,26 @@ public class Thread_communication
     // Attributs
 
     private int _numero_port;
+    private int _parties_gerees;
     
 
     // Constructeur
 
     public Thread_communication(int num_port){
         _numero_port = num_port;
+        _parties_gerees = 0;
     }
     
     // Getters et setters
 
-    
+    public int get_parties_gerees(){
+        return _parties_gerees;
+    }
+
+    // Augmente le nombre de parties gérées de 1
+    public int add_partie_geree(){
+        _parties_gerees++;
+    }
 
     // Méthodes
 
