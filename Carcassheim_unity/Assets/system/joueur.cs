c72a08a2e5b289251007b38e88458e0248e2fb6c
eizouki <issam.eizouki@etu.unistra.fr>
2022-03-07 18:49:40 +0100

    modif classe joueur

--- Carcassheim_unity/Assets/system/joueur.cs
+++ Carcassheim_unity/Assets/system/joueur.cs
@@ -3,13 +3,13 @@ public class joueur {
     public int _tricheJoueur;
     public int _noteJoueur;
     public int _pointsJoueur;
-    public int _abbé;
+    public int _abbe;
     public int _meeples;
     public void initJoueur(int id){
         _idjoueur=id;
         _tricheJoueur=0;    //initialisé à 0, incremente si triche, à 2 déconnecter joueur
         _noteJoueur=3;      //3 tours à rater avant déconnecter le joueur
-        _abbé=2;
+        _abbe=2;
         _meeples=5;
         _pointsJoueur=0;
     }
