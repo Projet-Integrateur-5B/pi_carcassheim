614c11beb4727905d9b000127445847b6ab017ae
eizouki <issam.eizouki@etu.unistra.fr>
2022-03-11 20:07:49 +0100

    LireXml testé

--- Carcassheim_unity/Assets/system/LireXml.cs
+++ Carcassheim_unity/Assets/system/LireXml.cs
@@ -2,90 +2,92 @@
 using System;
 using System.Xml;
 using System.Collections.Generic;
-
-class LireXml {
-    
-
-public LireXml(string file)
+using UnityEngine;
+class LireXml : MonoBehaviour
 {
 
-    int idTu = 0, idTe=0, idSl = 0, i=0,j=0,k=0;
-    List<Slot> slot = new List<Slot>(); //.ToArray()
-            //slot.Add pour ajouter un elem
-    int[][]lien= new int [12][];
+
+    public LireXml(string file)
+    {
+        int idTu = 0, idTe = 0, idSl = 0, i = 0, j = 0, nbPos = 0;
+        List<Slot> slot = new List<Slot>(); 
+       
+        int[][] lien = new int[12][];
         for (int n = 0; n < 12; n++)
         {
             lien[n] = new int[12];
         }
-    List<int> tab = new List<int>();
-    string nomTe="",tmp ="";
+        List<int> tab = new List<int>();
+        string nomTe = "", tmp = "";
 
-    using (XmlReader reader = XmlReader.Create(@file)){
-        while(reader.Read()){
-            if(reader.IsStartElement()){
-                switch (reader.Name.ToString())
+        using (XmlReader reader = XmlReader.Create(@file))
+        {
+            while (reader.Read())
+            {
+                if (reader.IsStartElement())
                 {
-                   case "terrain":
-                        reader.ReadStartElement("idTe"); 
-                        idTe = Int32.Parse(reader.ReadString());
-                        reader.ReadStartElement("NomTe"); //depend de l'écriture dans le fichier xml
-                        nomTe = reader.ReadString();
-                        reader.ReadEndElement();
-                        break;
+                    switch (reader.Name.ToString())
+                    {
+                        case "terrain":
+                            reader.ReadToFollowing("idTe");
+                            idTe = int.Parse(reader.ReadString());
+                            reader.ReadToFollowing("nomTe"); //depend de l'écriture dans le fichier xml
+                            nomTe = reader.ReadString();
+                            Debug.Log("Terrain : ");
+                            Debug.Log(idTe);
+                            Debug.Log(nomTe);
 
-                    case "tuile":
+                            break;
 
-                        reader.ReadStartElement("idTu"); 
-                        idTu = Int32.Parse(reader.ReadString());
+                        case "tuile":
 
-                        reader.ReadStartElement("slot");
-                            int nbAttr = reader.AttributeCount;
+                            reader.ReadToFollowing("idTu");
+                            idTu = int.Parse(reader.ReadString());
                             
+                            reader.ReadToFollowing("nbSlots");
+                            j = Int32.Parse(reader.ReadString());
 
-                            for (int attInd = 0; attInd < nbAttr-1; attInd++)
-                            {                             
-                            //Récupérer le tableau des positions internes (slot[])
-                            while(reader.MoveToNextAttribute())
+                            for (i = j; i > 0; i--)
+                            {
+                                reader.ReadToFollowing("slot");
+                                reader.ReadToFollowing("idSl");
+                                idSl = Int32.Parse(reader.ReadString());//id du slot
+
+                                reader.ReadToFollowing("nbPositions");
+                                nbPos = Int32.Parse(reader.ReadString());
+
+                                //Récupérer le tableau des positions internes (slot[])
+                                reader.ReadToFollowing("postionsSlot");
+                                tmp = reader.ReadString();
+                                string[] subs = tmp.Split(',', '.','\0');
+                                foreach (string sub in subs)
                                 {
-                                    tmp = reader.ReadString();
-                                    if (tmp == "terrain")
+                                   if (sub == "N" || sub == "NNO" || sub == "NNE" || sub == "NEE" || sub == "E" || sub == "SEE" || sub == "S" || sub == "SSO" || sub == "SOO" || sub == "O" || sub == "NOO")
                                     {
-                                        reader.ReadStartElement("terrain");
-                                        idTe = Int32.Parse(reader.ReadString());
-                                        slot.Add(new Slot(idTe));
-
+                                        tab.Add(Tuile.PointsCardPos[sub]);
                                     }
-                                    if (tmp == "idSl")
-                                    {
-                                        idSl = Int32.Parse(reader.ReadString());//id du slot
-
-                                    }
-                                    else
-                                    {
-                                        tab.Add(Tuile.PointsCardPos[tmp]);
-                                        j++;
-                                    }
-                                    reader.ReadEndElement();
-
                                 }
-
-                            }           
-                            
-   
-                            lien[idSl] = tab.ToArray();      //Ajouter le tableau des liens sémantique
-                            Tuile t= new Tuile(idTu,slot.ToArray(),lien);
-                            Tuile.DicoTuiles.Add(idTu,t); //Ajouter la tuile à la Dico
-
-                            ///tab 
-                            j = 0;
-                        break;
+                                
+                                reader.ReadToFollowing("terrain");
+                                idTe = Int32.Parse(reader.ReadString());
+                                slot.Add(new Slot(idTe));
+                            }
+                            lien[idSl] = tab.ToArray();      //Ajouter le tableau des liens sémantiques
+                            Tuile t = new Tuile(idTu, slot.ToArray(), lien);
+                            Tuile.DicoTuiles.Add(idTu, t); //Ajouter la tuile à la Dico
+                            break;
+                    }
                 }
-
+                //reader.ReadEndElement();
             }
         }
     }
-}
-
-
+   /*
+    void Start()
+    {
+        String file = "Assets/system/infos.xml";
+        LireXml l = new LireXml(file);
+    }
+   */
 }
 
