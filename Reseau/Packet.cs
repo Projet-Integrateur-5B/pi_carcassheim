2eb7425ed92647324993636b20a9076887c0160c
Zapharaos <mfreitag@numericable.fr>
2022-03-13 18:47:34 +0100

    edit following Justin's suggestions

--- Reseau/Packet.cs
+++ Reseau/Packet.cs
@@ -4,29 +4,24 @@ using System.Text;
 
 public class Packet
 {
-    public bool Type { get; } // 0 : client -> server ; 1 : server -> client
+    public bool Type { get; } // false (client -> server) - true (server -> client)
 
-    // type == 0 : client -> server
+    // type == false (client -> server)
     public IPAddress IpAddress { get; set; }
     public ushort Port { get; set; }
     public ulong IdRoom { get; set; } // default : 0 -> not destined to a room
     public byte IdMessage { get; set; } // à définir
 
-    // type == 1 : server -> client
+    // type == true (server -> client)
     public bool Status { get; set; } // 0 : error ; 1 : success
     public byte Permission { get; set; } // 0 : unused/error ; 1 : permission ; 2 : confirmation
 
     // common to both
     public ulong IdPlayer { get; set; }
-    public String Data { get; set; } // à définir
+    public string Data { get; set; } // à définir
 
-
-
-    // basic
-    public Packet() { }
-
-    // type == 0 : client -> server
-    public Packet(bool type, ushort port, IPAddress ipAddress, ulong idPlayer, ulong idRoom, byte idMessage, String data)
+    // type == false (client -> server)
+    public Packet(bool type, ushort port, IPAddress ipAddress, ulong idPlayer, ulong idRoom, byte idMessage, string data)
     {
         Type = type;
         IpAddress = ipAddress;
@@ -37,27 +32,14 @@ public class Packet
         Data = data;
     }
 
-    // type == 1 : server -> client
-    public Packet(bool type, bool status, byte permission, ulong idPlayer, String data)
+    // type == true (server -> client)
+    public Packet(bool type, bool status, byte permission, ulong idPlayer, string data)
     {
         Type = type;
         Status = status;
         Permission = permission;
         IdPlayer = idPlayer;
         Data = data;
-    }
-
-    public static void Main()
-    {
-        Packet sendPacket = new Packet(true, true, 1, 1, "test");
-
-        // serialize
-        byte[] packetAsBytes = sendPacket.Serialize();
-
-        // insert send & recv packet
-
-        // unserialize
-        Packet? recvPacket = Deserialize(packetAsBytes);
     }
 
     public byte[] Serialize()
@@ -69,7 +51,20 @@ public class Packet
 
     public static Packet Deserialize(byte[] packetAsBytes)
     {
-        String packetAsJson = Encoding.Default.GetString(packetAsBytes);
+        string packetAsJson = Encoding.Default.GetString(packetAsBytes);
         return JsonSerializer.Deserialize<Packet>(packetAsJson);
+    }
+    
+    static void Main()
+    {
+        Packet sendPacket = new Packet(true, true, 1, 1, "test");
+
+        // serialize
+        byte[] packetAsBytes = sendPacket.Serialize();
+
+        // insert send & recv packet
+
+        // unserialize
+        Packet? recvPacket = Deserialize(packetAsBytes);
     }
 }
