2a3bbcc0c3583d0fd21dd8057ff40a408ba2085c
Axel Grimmer <axel.grimmer@etu.unistra.fr>
2022-03-15 16:23:56 +0100

    Rajout d'un namespace

--- Carcassheim_unity/Assets/system/Thread_communication.cs
+++ Carcassheim_unity/Assets/system/Thread_communication.cs
@@ -3,139 +3,149 @@ using System.Threading;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Thread_communication
+namespace system
 {
-    // Attributs
 
-    private int _id_thread_com;
-    private int _numero_port;
-    private int _nb_parties_gerees;
-
-    private List<int> _id_parties_gerees;
-    private List<Thread_serveur_jeu> _lst_serveur_jeu;
-
-    private static int _compteur_id_thread_com;
-
-    // Locks
-
-    private readonly object _lock_nb_parties_gerees;
-    private readonly object _lock_id_parties_gerees;
-
-    // Rajouter un objet RESEAU pour les communications ?
-
-
-    // Constructeur
-
-    public Thread_communication(int num_port, int id){
-        _numero_port = num_port;
-        _nb_parties_gerees = 0;
-        _id_parties_gerees = new List<int>();
-        _id_thread_com = id;
-        _lock_nb_parties_gerees = new object();
-        _lock_id_parties_gerees = new object();
-    }
-    
-    // Getters et setters
-
-    public int Get_nb_parties_gerees(){
-        return _nb_parties_gerees;
-    }
-
-    public List<int> Get_id_parties_gerees(){
-        return _id_parties_gerees;
-    }
-
-    public object Get_lock_nb_parties_gerees()
+    public class Thread_communication
     {
-        return _lock_nb_parties_gerees;
-    }
+        // Attributs
 
-    public object Get_lock_id_parties_gerees()
-    {
-        return _lock_id_parties_gerees;
-    }
+        private int _id_thread_com;
+        private int _numero_port;
+        private int _nb_parties_gerees;
 
-    // Augmente le nombre de parties gérées de 1
-    public void Add_partie_geree(int id_partie_ajoutee){
-        _id_parties_gerees.Add(id_partie_ajoutee);
-        _nb_parties_gerees++;
-    }
+        private List<int> _id_parties_gerees;
+        private List<Thread_serveur_jeu> _lst_serveur_jeu;
 
-    // Création d'un nouveau thread_serveur_jeu
+        private static int _compteur_id_thread_com;
 
-    // Méthodes
+        // Locks
 
-    public void Lancement_thread_com(){
+        private readonly object _lock_nb_parties_gerees;
+        private readonly object _lock_id_parties_gerees;
 
-        Thread.Sleep(2000);
+        // Rajouter un objet RESEAU pour les communications ?
 
-        Debug.Log(string.Format("[{0}] Je suis un thread !", _id_thread_com));
-        Debug.Log(string.Format("[{0}] J'officie sur le port numéro {1} !", _id_thread_com, _numero_port));
-        Debug.Log(string.Format("[{0}] Je gère actuellement {1} parties!", _id_thread_com, _nb_parties_gerees));
-        foreach(int id_ite in _id_parties_gerees){
-            Debug.Log(string.Format("[{0}] Je gère la partie d'ID {1}", _id_thread_com, id_ite));
+
+        // Constructeur
+
+        public Thread_communication(int num_port, int id)
+        {
+            _numero_port = num_port;
+            _nb_parties_gerees = 0;
+            _id_parties_gerees = new List<int>();
+            _id_thread_com = id;
+            _lock_nb_parties_gerees = new object();
+            _lock_id_parties_gerees = new object();
         }
 
-        //Debug.Log(string.Format("Compteur d'id de strings : {0}", _compteur_id_thread_com));
+        // Getters et setters
 
-
-        int debug = 1;
-
-        if(debug != 1) // TEMPORAIRE - A retirer plus tard    // A REMPLACER - Par boucle de réception
+        public int Get_nb_parties_gerees()
         {
-            // Types de messages qui peuvent être reçus:
-            //      • Création de partie
-            //      • Arrivée dans la partie
-            //      • Pose de tuile
-            //      • Pose de pion
-            //      • ? Fin de tour ?
+            return _nb_parties_gerees;
+        }
 
-            // Types de message à envoyer spontanéement par le thread_com:
-            //      • Fin de partie
-            //      • Réaction à triche
-            //      • Réaction à afk
-            //      • Kick (afk ou triche)
+        public List<int> Get_id_parties_gerees()
+        {
+            return _id_parties_gerees;
+        }
 
+        public object Get_lock_nb_parties_gerees()
+        {
+            return _lock_nb_parties_gerees;
+        }
 
-            int typeMsg = 0; // (Dépend du RESEAU) Dépendra du type : création de partie, connexion à la partie, etc
-            int portPartie = 0; // (Dépend du RESEAU) Port de la partie en question
-            int id_joueur_client = 0; // (Dépend du RESEAU)
+        public object Get_lock_id_parties_gerees()
+        {
+            return _lock_id_parties_gerees;
+        }
 
-            if (typeMsg == 1)    // Création de partie
+        // Augmente le nombre de parties gérées de 1
+        public void Add_partie_geree(int id_partie_ajoutee)
+        {
+            _id_parties_gerees.Add(id_partie_ajoutee);
+            _nb_parties_gerees++;
+        }
+
+        // Création d'un nouveau thread_serveur_jeu
+
+        // Méthodes
+
+        public void Lancement_thread_com()
+        {
+
+            Thread.Sleep(2000);
+
+            Debug.Log(string.Format("[{0}] Je suis un thread !", _id_thread_com));
+            Debug.Log(string.Format("[{0}] J'officie sur le port numéro {1} !", _id_thread_com, _numero_port));
+            Debug.Log(string.Format("[{0}] Je gère actuellement {1} parties!", _id_thread_com, _nb_parties_gerees));
+            foreach (int id_ite in _id_parties_gerees)
             {
-                int id_nouv_partie = -1;
+                Debug.Log(string.Format("[{0}] Je gère la partie d'ID {1}", _id_thread_com, id_ite));
+            }
 
-                lock (this.Get_lock_nb_parties_gerees())
+            //Debug.Log(string.Format("Compteur d'id de strings : {0}", _compteur_id_thread_com));
+
+
+            int debug = 1;
+
+            if (debug != 1) // TEMPORAIRE - A retirer plus tard    // A REMPLACER - Par boucle de réception
+            {
+                // Types de messages qui peuvent être reçus:
+                //      • Création de partie
+                //      • Arrivée dans la partie
+                //      • Pose de tuile
+                //      • Pose de pion
+                //      • ? Fin de tour ?
+
+                // Types de message à envoyer spontanéement par le thread_com:
+                //      • Fin de partie
+                //      • Réaction à triche
+                //      • Réaction à afk
+                //      • Kick (afk ou triche)
+
+
+                int typeMsg = 0; // (Dépend du RESEAU) Dépendra du type : création de partie, connexion à la partie, etc
+                int portPartie = 0; // (Dépend du RESEAU) Port de la partie en question
+                int id_joueur_client = 0; // (Dépend du RESEAU)
+
+                if (typeMsg == 1)    // Création de partie
                 {
-                    if (_nb_parties_gerees < 5)
+                    int id_nouv_partie = -1;
+
+                    lock (this.Get_lock_nb_parties_gerees())
                     {
-                        id_nouv_partie = 1; // TEMPORAIRE     //  BDD - Fonction de récupération d'ID libre
+                        if (_nb_parties_gerees < 5)
+                        {
+                            id_nouv_partie = 1; // TEMPORAIRE     //  BDD - Fonction de récupération d'ID libre
 
-                        this.Add_partie_geree(id_nouv_partie);
+                            this.Add_partie_geree(id_nouv_partie);
+                        }
                     }
+
+                    if (id_nouv_partie != -1) // Si la partie a pu être crée
+                    {
+                        Thread_serveur_jeu thread_serveur_jeu = new Thread_serveur_jeu(id_nouv_partie, id_joueur_client);
+                        Thread nouv_thread = new Thread(new ThreadStart(thread_serveur_jeu.Lancement_thread_serveur_jeu));
+
+                        _lst_serveur_jeu.Add(thread_serveur_jeu);
+
+                        nouv_thread.Start();
+
+                        // A FAIRE - Rajouter ce joueur dans la partie
+                    }
+                    else
+                    {
+                        // RESEAU - Fonction qui indique au client que la partie n'a pas pu être créée 
+                    }
+
                 }
 
-                if (id_nouv_partie != -1) // Si la partie a pu être crée
-                {
-                    Thread_serveur_jeu thread_serveur_jeu = new Thread_serveur_jeu(id_nouv_partie, id_joueur_client);
-                    Thread nouv_thread = new Thread(new ThreadStart(thread_serveur_jeu.Lancement_thread_serveur_jeu));
 
-                    _lst_serveur_jeu.Add(thread_serveur_jeu);
-
-                    nouv_thread.Start();
-
-                    // A FAIRE - Rajouter ce joueur dans la partie
-                }
-                else
-                {
-                    // RESEAU - Fonction qui indique au client que la partie n'a pas pu être créée 
-                }
-                
             }
 
 
         }
-
- 
     }
 }
--- Carcassheim_unity/Assets/system/Thread_serveur_jeu.cs
+++ Carcassheim_unity/Assets/system/Thread_serveur_jeu.cs
@@ -1,65 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
-public partial class Thread_serveur_jeu
+namespace system
 {
 
-	// Attributs
-
-	// Champs qui paramètrent la partie
-
-	private readonly int _id_partie;
-
-	private Dictionary<int, int> _dico_joueur_score; // Contient les ID's de chaque joueur
-	private int _id_moderateur; // Identifiant du joueur modérateur
-
-	private string _statut_partie;
-
-	private int _mode; // 0 -> Classique | 1 -> Time-attack | 2 -> Score
-
-	private int _nb_tuiles;
-	private int _score_max;
-
-	private bool _privee;
-	private int _timer; // En secondes
-	private int _timer_max_joueur; // En secondes
-	private int _meeples; // Nombre de meeples par joueur
-
-
-	// Champs nécessaires pour le bon fonctionnement du programme
-
-
-
-	// Constructeur
-	public Thread_serveur_jeu(int id_partie, int id_joueur_createur)
+	public partial class Thread_serveur_jeu
 	{
-		_id_partie = id_partie;
 
-		_dico_joueur_score = new Dictionary<int, int>();
+		// Attributs
 
-		_dico_joueur_score.Add(id_joueur_createur,0);
-		_id_moderateur = id_joueur_createur;
+		// Champs qui paramètrent la partie
 
-		_statut_partie = "ACCUEIL";
+		private readonly int _id_partie;
 
-		// Initialisation des valeurs par défaut
-		_mode = 0;
-		_nb_tuiles = 60;
-		_score_max = -1;
-		_privee = true; // Une partie est par défaut privée
-		_timer = 3600; // Une heure par défaut
-		_timer_max_joueur = 40;
-		_meeples = 8;
+		private Dictionary<int, int> _dico_joueur_score; // Contient les ID's de chaque joueur
+		private int _id_moderateur; // Identifiant du joueur modérateur
+
+		private string _statut_partie;
+
+		private int _mode; // 0 -> Classique | 1 -> Time-attack | 2 -> Score
+
+		private int _nb_tuiles;
+		private int _score_max;
+
+		private bool _privee;
+		private int _timer; // En secondes
+		private int _timer_max_joueur; // En secondes
+		private int _meeples; // Nombre de meeples par joueur
+
+
+		// Champs nécessaires pour le bon fonctionnement du programme
+
+
+
+		// Constructeur
+		public Thread_serveur_jeu(int id_partie, int id_joueur_createur)
+		{
+			_id_partie = id_partie;
+
+			_dico_joueur_score = new Dictionary<int, int>();
+
+			_dico_joueur_score.Add(id_joueur_createur, 0);
+			_id_moderateur = id_joueur_createur;
+
+			_statut_partie = "ACCUEIL";
+
+			// Initialisation des valeurs par défaut
+			_mode = 0;
+			_nb_tuiles = 60;
+			_score_max = -1;
+			_privee = true; // Une partie est par défaut privée
+			_timer = 3600; // Une heure par défaut
+			_timer_max_joueur = 40;
+			_meeples = 8;
+		}
+
+		// Méthodes
+
+		public void Lancement_thread_serveur_jeu()
+		{
+
+
+
+			// A FAIRE - Fin/retour de la fonction pour libérer l'objet associé dans le thread_com à la fin de la partie et donc de ce thread
+		}
 	}
 
-	// Méthodes
-
-	public void Lancement_thread_serveur_jeu()
-    {
-
-		
-
-		// A FAIRE - Fin/retour de la fonction pour libérer l'objet associé dans le thread_com à la fin de la partie et donc de ce thread
-    }
 }
