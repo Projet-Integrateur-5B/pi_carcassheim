9dc878fc626429fe6c833fad64d02162c7c81cb0
Zapharaos <mfreitag@numericable.fr>
2022-03-23 21:09:11 +0100

    tentative serialize server

--- Appli_serveur_test/Appli_serveur_test/Tools.cs
+++ Appli_serveur_test/Appli_serveur_test/Tools.cs
@@ -1,17 +1,26 @@
 namespace Server;
 
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Text.Json;
 using Assets;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
 
 public static class Tools
 {
+
     public static byte[] PacketToByteArray(this Packet? packet)
     {
-        var jso = new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
-        var jsonString = JsonSerializer.Serialize(packet, jso);
-        return Encoding.ASCII.GetBytes(jsonString);
+        if (packet == null)
+        {
+            return Array.Empty<byte>();
+        }
+        var bf = new BinaryFormatter();
+        using (var ms = new MemoryStream())
+        {
+            bf.Serialize(ms, packet);
+            return ms.ToArray();
+        }
     }
 
     public static Packet ByteArrayToPacket(this byte[]? byteArray)
@@ -20,8 +29,14 @@ public static class Tools
         {
             return new Packet();
         }
-        var packetAsJson = Encoding.ASCII.GetString(byteArray);
-        return JsonSerializer.Deserialize<Packet>(packetAsJson) ?? new Packet();
+        using (var memStream = new MemoryStream())
+        {
+            var binForm = new BinaryFormatter();
+            memStream.Write(byteArray, 0, byteArray.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            var obj = (Packet)binForm.Deserialize(memStream);
+            return obj;
+        }
     }
 }
 
