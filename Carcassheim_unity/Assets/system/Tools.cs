dff150c651613765b3004c0c71d6e8f26ec3f65a
Zapharaos <mfreitag@numericable.fr>
2022-03-24 16:03:05 +0100

    fix packet prepare tool

--- Carcassheim_unity/Assets/system/Tools.cs
+++ Carcassheim_unity/Assets/system/Tools.cs
@@ -21,7 +21,6 @@ public static class Tools
     public static List<Packet> Prepare(this Packet original)
     {
         var packets = new List<Packet>();
-        original ??= new Packet();
 
         // within authorized range of size
         if (original.PacketToByteArray().Length < Packet.MaxPacketSize)
@@ -30,16 +29,14 @@ public static class Tools
             return packets;
         }
 
+        var dataString = original.Data;
         var header = original; // copy the original packet
         header.Data = ""; // empty the data field (= keep the packet's header)
 
         var headerBytes = header.PacketToByteArray(); // header to bytes
         var headerBytesLength = headerBytes.Length; // length
-
         var headerBytesMaxLength = Packet.MaxPacketSize - headerBytesLength; // max length allowed
-        var dataString = JsonConvert.SerializeObject(original.Data);
-        dataString = dataString.Substring(1, dataString.Length - 2);
-        // test = test.Substring(1, test.Length - 2);
+
         var dataBytes = Encoding.ASCII.GetBytes(dataString);
         var dataBytesTotalLength = dataBytes.Length;
 
@@ -48,9 +45,9 @@ public static class Tools
             var packet = headerBytes.ByteArrayToPacket();
             if (i + headerBytesMaxLength > dataBytesTotalLength)
             {
-                packet.Data = dataString.Substring(i, dataBytesTotalLength-i);
-                Console.WriteLine(dataString.Substring(i,dataBytesTotalLength-i));
-                // packet.Data = dataString.Substring(i, dataBytesTotalLength-i);
+                packet.Data = dataString.Substring(i, dataBytesTotalLength - i);
+                Console.WriteLine(dataString.Substring(i, dataBytesTotalLength - i));
+                // dataString.Substring(i, dataBytesTotalLength - i);
             }
             else
             {
