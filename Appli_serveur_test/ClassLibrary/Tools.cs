aec5634c96cd7e0629375c712bb28a510f03330e
Zapharaos <mfreitag@numericable.fr>
2022-03-24 13:46:19 +0100

    fix serialize on server side

--- Appli_serveur_test/ClassLibrary/Tools.cs
+++ Appli_serveur_test/ClassLibrary/Tools.cs
@@ -1,27 +1,22 @@
 namespace ClassLibrary;
 
 using System.Text;
-using System.Text.Encodings.Web;
-using System.Text.Json;
+using Newtonsoft.Json;
 
 public static class Tools
 {
-    public static byte[] PacketToByteArray(this Packet? packet)
+    public static byte[] PacketToByteArray(this Packet packet)
     {
-        var jso = new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
-        var jsonString = JsonSerializer.Serialize(packet, jso);
-        return Encoding.ASCII.GetBytes(jsonString);
+        var serialized = JsonConvert.SerializeObject(packet);
+        return Encoding.ASCII.GetBytes(serialized);
     }
 
-    public static Packet ByteArrayToPacket(this byte[]? byteArray)
+    public static Packet ByteArrayToPacket(this byte[] byteArray)
     {
-        if (byteArray == null)
-        {
-            return new Packet();
-        }
         var packetAsJson = Encoding.ASCII.GetString(byteArray);
-        return JsonSerializer.Deserialize<Packet>(packetAsJson) ?? new Packet();
+        return JsonConvert.DeserializeObject<Packet>(packetAsJson) ?? new Packet();
     }
+
 }
 
 
