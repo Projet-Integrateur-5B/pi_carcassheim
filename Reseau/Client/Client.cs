81ea9cbc94163e56a8442b0abe880fe6396795bd
Zapharaos <mfreitag@numericable.fr>
2022-03-26 19:08:14 +0100

    reset Client.cs file

--- Reseau/Client/Client.cs
+++ Reseau/Client/Client.cs
@@ -5,10 +5,10 @@ public partial class Client
     public static int Main()
     {
         var sender = Connect.Connection();
-        Communication(sender, 1, "abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz");
-        /*Communication(sender, 5, "test deux");
+        Communication(sender, 1, "petit test");
+        Communication(sender, 5, "test deux");
         Communication(sender, 9, "test trois");
-        Communication(sender, 2, "test trois");*/
+        Communication(sender, 2, "test trois");
         Disconect.Deconnection(sender);
         return 0;
     }
