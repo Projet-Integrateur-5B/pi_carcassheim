0aa065768656dae64353529707b5cc8eae1e2cc4
Geissert Cedric <cedric.geissert@etu.unistra.fr>
2022-03-27 20:12:16 +0200

    Redesign font, button, hover -> all menus done and sound/music working + debug done => Need to improve Hover : Back/ShowCreateRoom/ShowRoomParameters & Trident

--- Carcassheim_unity/Assets/Menu/Resources/Scripts/OptionsMenu.cs
+++ Carcassheim_unity/Assets/Menu/Resources/Scripts/OptionsMenu.cs
@@ -5,7 +5,7 @@ public class OptionsMenu : Miscellaneous
 {
 	private Button _btnSon, _btnMusique;
 	private AudioSource _soundCtrl, _musicCtrl;
-	private Scrollbar _soundScroll, _musicScroll;
+	private Slider _soundSlider, _musicSlider;
 	private Text _pourcentSon, _pourcentMusique;
 	private float _previousSoundVol = 0.0f;
 	private float _previousMusicVol = 0.0f;
@@ -13,32 +13,29 @@ public class OptionsMenu : Miscellaneous
 	float lastMusicValue = 0;
 	public Toggle toggle_french, toggle_english, toggle_german;
 	private GameObject containerButtons;
-	private Transform optionsMenu, OCB; // Options Container Buttons
+	private Transform optionsMenu, OCB, OCS, OCT; // Options Container Buttons
 	// Start is called before the first frame update
 	void Start()
 	{
 		// INITIALISATION
 		optionsMenu = GameObject.Find("SubMenus").transform.Find("OptionsMenu").transform;
 		OCB = optionsMenu.Find("Buttons").transform;
-		_soundScroll = optionsMenu.Find("Scrollbar Son").GetComponent<Scrollbar>();
-		_musicScroll = optionsMenu.Find("Scrollbar Musique").GetComponent<Scrollbar>();
-		_pourcentMusique = optionsMenu.Find("Pourcent Musique").GetComponent<Text>();
-		_pourcentSon = optionsMenu.Find("Pourcent Son").GetComponent<Text>();
+		OCS = optionsMenu.Find("Sliders").transform;
+		OCT = optionsMenu.Find("Text").transform;
+		_soundSlider = OCS.Find("Slider Son").GetComponent<Slider>();
+		_musicSlider = OCS.Find("Slider Musique").GetComponent<Slider>();
+		_pourcentSon = OCT.Find("Pourcent Son").GetComponent<Text>();
+		_pourcentMusique = OCT.Find("Pourcent Musique").GetComponent<Text>();
 		_btnMusique = OCB.Find("SwitchMusic").GetComponent<Button>();
 		_btnSon = OCB.Find("SwitchSound").GetComponent<Button>();
 		_soundCtrl = GameObject.Find("SoundController").GetComponent<AudioSource>();
 		_musicCtrl = GameObject.Find("MusicController").GetComponent<AudioSource>();
 		DefaultMusicSound();
-		//Subscribe to the Scrollbar event
-		_soundScroll.onValueChanged.AddListener(SoundScrollbarCallBack);
-		lastSoundValue = _soundScroll.value;
-		_musicScroll.onValueChanged.AddListener(MusicScrollbarCallBack);
-		lastMusicValue = _musicScroll.value;
-	/*	toggle_french = GameObject.Find("Toggle French").GetComponent<Toggle>();
-		toggle_french.onValueChanged.AddListener(delegate
-		{
-			ToggleValueChanged(toggle_french);
-		}); */
+		//Subscribe to the Slider event
+		_soundSlider.onValueChanged.AddListener(SoundSliderCallBack);
+		lastSoundValue = _soundSlider.value;
+		_musicSlider.onValueChanged.AddListener(MusicSliderCallBack);
+		lastMusicValue = _musicSlider.value;
 	}
 
 	public void ToggleValueChangedOM(Toggle curT)
@@ -48,7 +45,7 @@ public class OptionsMenu : Miscellaneous
 	}
 
 	//---------------------------- Music/Sound Begin ----------------------------//
-	public void Volume(AudioSource ads, Text txt, Scrollbar sb)
+	public void Volume(AudioSource ads, Text txt, Slider sb)
 	{
 		ads.volume = sb.value;
 		txt.text = Mathf.RoundToInt(sb.value * 100) + "%";
@@ -56,67 +53,75 @@ public class OptionsMenu : Miscellaneous
 
 	public void DisplayVolume(int b, float value)
 	{
+		GameObject g = _btnMusique.gameObject;
+		GameObject gs = _btnSon.gameObject;
+		bool tmpS = true; 
+		bool tmpM = true;
 		if (value > 0)
 			if (b == 0)
-				_btnSon.GetComponentInChildren<Text>().text = "Son 'ON'";
-			else
-				_btnMusique.GetComponentInChildren<Text>().text = "Musique 'ON'";
+				tmpS = true;
+			else 
+				tmpM = true;
 		else if (b == 0)
-			_btnSon.GetComponentInChildren<Text>().text = "Son 'OFF'";
-		else
-			_btnMusique.GetComponentInChildren<Text>().text = "Musique 'OFF'";
+				tmpS = false;
+			else 
+				tmpM = false;
+		gs.transform.Find("SonON").gameObject.SetActive(tmpS);
+		gs.transform.Find("SonOFF").gameObject.SetActive(!tmpS);
+		g.transform.Find("MusicON").gameObject.SetActive(tmpM);
+		g.transform.Find("MusicOFF").gameObject.SetActive(!tmpM);
 		if (b == 0)
-			Volume(_soundCtrl, _pourcentSon, _soundScroll);
+			Volume(_soundCtrl, _pourcentSon, _soundSlider);
 		else
-			Volume(_musicCtrl, _pourcentMusique, _musicScroll);
+			Volume(_musicCtrl, _pourcentMusique, _musicSlider);
 	}
 
 	public void DefaultMusicSound()
 	{
-		_soundScroll.numberOfSteps = _musicScroll.numberOfSteps = 11; // 0->10 = 11
-		_soundCtrl.volume = _musicCtrl.volume = _soundScroll.value = _musicScroll.value = 0.2f;
-		Volume(_soundCtrl, _pourcentSon, _soundScroll);
-		Volume(_musicCtrl, _pourcentMusique, _musicScroll);
+		_soundSlider.maxValue = _musicSlider.maxValue = 1; 
+		_soundCtrl.volume = _musicCtrl.volume = _soundSlider.value = _musicSlider.value = 0.4f;
+		Volume(_soundCtrl, _pourcentSon, _soundSlider);
+		Volume(_musicCtrl, _pourcentMusique, _musicSlider);
 	}
 
 	//Will be called when Scrollbar changes
-	public void SoundScrollbarCallBack(float value)
+	public void SoundSliderCallBack(float value)
 	{
 		DisplayVolume(0, value);
 	}
 
 	//Will be called when Scrollbar changes
-	public void MusicScrollbarCallBack(float value)
+	public void MusicSliderCallBack(float value)
 	{
 		DisplayVolume(1, value);
 	}
 
 	public void SwitchSound()
 	{
-		if (_soundScroll.value != 0)
+		if (_soundSlider.value != 0)
 		{
 			_previousSoundVol = _soundCtrl.volume;
-			_soundScroll.value = 0.0f;
-			DisplayVolume(0, _soundScroll.value);
+			_soundSlider.value = 0.0f;
+			DisplayVolume(0, _soundSlider.value);
 		}
 		else
 		{
-			_soundScroll.value = _previousSoundVol;
+			_soundSlider.value = _previousSoundVol;
 			DisplayVolume(0, _previousSoundVol);
 		}
 	}
 
 	public void SwitchMusic()
 	{
-		if (_musicScroll.value != 0)
+		if (_musicSlider.value != 0)
 		{
 			_previousMusicVol = _musicCtrl.volume;
-			_musicScroll.value = 0.0f;
-			DisplayVolume(1, _musicScroll.value);
+			_musicSlider.value = 0.0f;
+			DisplayVolume(1, _musicSlider.value);
 		}
 		else
 		{
-			_musicScroll.value = _previousMusicVol;
+			_musicSlider.value = _previousMusicVol;
 			DisplayVolume(1, _previousMusicVol);
 		}
 	}
