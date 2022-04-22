0649ba35768d12b695847b7ea3ff98bc6ed384dc
eizouki <issam.eizouki@etu.unistra.fr>
2022-04-22 14:29:48 +0200

    Méthode qui mélange la liste de tuiles

---
+++ Carcassheim_unity/Assets/system/RandomMixTuiles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+
+/* 
+ * Cette méthode est à utiliser côté serveur
+ * Dans le cas ou', au moment de l'envoie de trois tuiles 
+ * a echoué (aucune tiule n'est posable
+ * Dans ce cas : il faut appeler cette méthode afin de mélanger la listes des tuiles aavant de 
+ * appeler à nouveau la méthode qui tire trois tuiles
+ * elle prend en parametres la listeDesTuiles et la renvoie mélangée
+ * */
+public class RandomMixTuiles 
+{
+    public List<ulong> mixList(List<ulong> tuilesGame)
+    {
+        List<ulong> tuilesGame_resultat = new List<ulong>();
+        var rnd = new System.Random();
+        var randomedList = tuilesGame.OrderBy(item => rnd.Next());
+        foreach (var value in randomedList)
+        {
+            tuilesGame_resultat.Add(value);
+        }
+        return tuilesGame_resultat;
+    }
+
+    
+}
