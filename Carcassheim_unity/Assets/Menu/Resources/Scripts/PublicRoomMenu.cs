974e0bf31b33c0603617a792eee86caa16cb4d66
Geissert Cedric <cedric.geissert@etu.unistra.fr>
2022-04-24 15:08:22 +0200

    pret à jouer public color room menu

--- Carcassheim_unity/Assets/Menu/Resources/Scripts/PublicRoomMenu.cs
+++ Carcassheim_unity/Assets/Menu/Resources/Scripts/PublicRoomMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class PublicRoomMenu : Miscellaneous
 {
+	private Color readyState;
 	void Start()
 	{
 		
@@ -31,12 +32,15 @@ public class PublicRoomMenu : Miscellaneous
 	}
 
 	public void Ready(){
+		ColorUtility.TryParseHtmlString("#90EE90", out readyState);
+		Text preparer = Miscellaneous.FindObject(absolute_parent, "preparation").GetComponent<Text>();
 		//Application.OpenURL("https://tinyurl.com/Kakyoin-and-Polnareff");
 		Packet packet = new Packet();
 		packet.IdMessage = Tools.IdMessage.StartGame;
 		packet.IdPlayer = Communication.Instance.idClient;
 		packet.Data = new[] {Communication.Instance.idRoom.ToString()};
-        Miscellaneous.FindObject(absolute_parent, "preparation").GetComponent<Text>().text = "PRET A JOUER !";
+		preparer.color = readyState;
+        preparer.text = "PRET A JOUER !";
 		Communication.Instance.SetIsInRoom(1);
 		Communication.Instance.SendAsync(packet);
 	}
