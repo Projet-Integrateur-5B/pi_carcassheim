75607f16bf43a2bf2c45d5312950e287adf8eff9
Axel Grimmer <axel.grimmer@etu.unistra.fr>
2022-04-24 12:38:44 +0200

    Correction accolade manquante dans playerJoin

--- Appli_serveur_test/Appli_serveur_test/GestionnaireThreadCom.cs
+++ Appli_serveur_test/Appli_serveur_test/GestionnaireThreadCom.cs
@@ -286,10 +286,13 @@ namespace system
                     if (idRoom != thread_serv_ite.Get_ID().ToString()) continue;
                     var playerStatus = thread_serv_ite.AddJoueur(idPlayer, playerSocket);
                     if (playerStatus != Tools.PlayerStatus.Success) // Le joueur n'a pas pu être ajouté
-                        if(playerStatus == Tools.PlayerStatus.Found) // Le joueur est déjà dans la partie
+                    {
+                        if (playerStatus == Tools.PlayerStatus.Found) // Le joueur est déjà dans la partie
                             return thread_com_iterateur.Get_port() + 9000;
                         else // Tous les autres cas
                             return -1;
+                    }
+                        
 
                     return thread_com_iterateur.Get_port() + 9000; 
                 }
