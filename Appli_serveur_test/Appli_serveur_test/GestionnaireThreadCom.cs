899b80586ddb4b229235c5aedd9df103c923c01c
Axel Grimmer <axel.grimmer@etu.unistra.fr>
2022-04-25 15:32:55 +0200

    Ajout sleep entre les comm de début de game pour éviter l'embouteillage

--- Appli_serveur_test/Appli_serveur_test/GestionnaireThreadCom.cs
+++ Appli_serveur_test/Appli_serveur_test/GestionnaireThreadCom.cs
@@ -445,8 +445,10 @@ namespace system
                         string[] dataToSend = new string[] { thread_serv_ite.Get_idTuileInit().ToString(), 0.ToString(),
                             0.ToString(), 0.ToString() };
                         thread_com_iterateur.SendBroadcast(idRoom, Tools.IdMessage.TuilePlacement, dataToSend);
+                        Thread.Sleep(200);
                         // Préviens tous les joueurs (broadcast start)
                         thread_com_iterateur.SendBroadcast(idRoom, Tools.IdMessage.StartGame);
+                        Thread.Sleep(200);
                         // Envoi des 3 tuiles de début de tour
                         thread_com_iterateur.SendBroadcast(idRoom, Tools.IdMessage.TuileDraw, thread_serv_ite.GetThreeLastTiles());
                         return Tools.Errors.None; // return valeur correcte
