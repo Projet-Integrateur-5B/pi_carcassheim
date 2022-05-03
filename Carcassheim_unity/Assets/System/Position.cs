2a166b8c8102ffe80b894b27524255dedcc73e50
Justin F <justin.filipozzi@etu.unistra.fr>
2022-05-03 14:54:57 +0200

    doc Position.cs

--- Carcassheim_unity/Assets/System/Position.cs
+++ Carcassheim_unity/Assets/System/Position.cs
@@ -1,13 +1,36 @@
 ﻿namespace Assets.system
 {
+    /// <summary>
+    /// definie la position d'une tuile posee sur le plateau
+    /// </summary>
     public struct Position
     {
+        /// <summary>
+        /// definie la position
+        /// </summary>
         int _x, _y, _rot;
 
+        /// <summary>
+        /// l'abscisse de la tuile sur le plateau
+        /// </summary>
         public int X => _x;
+
+        /// <summary>
+        /// l'ordonnee de la tuile sur le plateau
+        /// </summary>
         public int Y => _y;
+
+        /// <summary>
+        /// la rotation de la tuile
+        /// </summary>
         public int ROT => _rot;
 
+        /// <summary>
+        /// instancie une position
+        /// </summary>
+        /// <param name="x">abscisse</param>
+        /// <param name="y">ordonnee</param>
+        /// <param name="rot">rotation</param>
         public Position(int x, int y, int rot)
         {
             _x = x;
@@ -15,6 +38,10 @@
             _rot = rot;
         }
 
+        /// <summary>
+        /// override pour faciliter le deboggage
+        /// </summary>
+        /// <returns>une chaine de la forme (x, y) [point cardinal correspondant a la rotation]</returns>
         public override string ToString()
         {
             string r = "";
