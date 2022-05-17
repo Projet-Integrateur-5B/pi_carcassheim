0d20a9cefdd5c74df3bef10f555d8229b796881f
Axel Grimmer <axel.grimmer@etu.unistra.fr>
2022-05-17 13:32:45 +0200

    Retrait de l'affichage des rooms déjà lancées

--- Appli_serveur_test/Appli_serveur_test/GestionnaireThreadCom.cs
+++ Appli_serveur_test/Appli_serveur_test/GestionnaireThreadCom.cs
@@ -138,7 +138,8 @@ namespace system
             {
                 foreach (Thread_serveur_jeu thread_serv_ite in thread_com_iterateur.Get_list_server_thread())
                 {
-                    if (thread_serv_ite.Is_Private() == false && thread_serv_ite.Get_Status() != Tools.GameStatus.Stopped)
+                    if (thread_serv_ite.Is_Private() == false && thread_serv_ite.Get_Status() != Tools.GameStatus.Stopped
+                        && thread_serv_ite.Get_Status() != Tools.GameStatus.Running)
                     {
                         room_list.Add(thread_serv_ite.Get_ID().ToString());
                         room_list.Add(thread_serv_ite.Get_Moderateur().ToString());
